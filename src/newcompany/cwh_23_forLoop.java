c42a9af2c26360d2289df75df380223c3735573a
Amey Joshi <ameyjoshi2001rj@gmail.com>
2022-08-10 16:46:43 +0530

    java practice code

---
+++ src/newcompany/cwh_23_forLoop.java
@@ -0,0 +1,20 @@
+package newcompany;
+
+public class cwh_23_forLoop {
+    public static void main(String[] args) {
+//     for(int i = 0;i<=10;i++)
+//     {
+//         System.out.println(i);
+//     }
+     // short cut key is ctrl + shift + enter
+        // if we want right a code syntactically correct then use the above shortcut to complete it correctly , like auto type
+//       int n = 5;
+//        for(int i=0;i<n;i++) {
+//            System.out.println(i);
+//        }
+        for (int i = 5;i!=0;i--) {
+            System.out.println(i);
+        }
+
+    }
+}
---
+++ src/newcompany/cwh_25_practice_set.java
@@ -0,0 +1,30 @@
+package newcompany;
+
+public class cwh_25_practice_set {
+    public static void main(String[] args) {
+        //Practice Problem 1
+//        int n = 4;
+//        for (int i =n;i>0;i--) {
+//            for (int j = 0;j<i;j++) {
+//                System.out.print("*");
+//            }
+//
+//            System.out.println("\n");
+//        }
+
+        //Practice Problem 2
+        int sum = 0;
+        int n = 4;
+        for (int i = 0;i<n;i++) {
+            sum = sum + (2*i);
+
+        }
+        System.out.print("Sum of even numbers is :");
+        System.out.print(sum);
+        //first 4 even number is 0 2 4 6
+
+        //Practice Problem 3
+
+
+    }
+}
