7c778533d5758b570ecc51e262d678dea32440ea
Amey Joshi <ameyjoshi2001rj@gmail.com>
2022-08-13 23:30:45 +0530

    java practice code solved new problems

--- src/newcompany/cwh_25_practice_set.java
+++ src/newcompany/cwh_25_practice_set.java
@@ -50,14 +50,46 @@ public class cwh_25_practice_set {
 //        System.out.println(factorial);
 
         //Practice Problem 7
-        int n = 4;
-        for (int i =n;i>0;i--) {
-            for (int j = 0;j<i;j++) {
-                System.out.print("*");
-            }
+        //using while loop
+//        int i = 1;
+//        while (i<=10) {
+//            int j = 1;
+//            while (j<=i){
+//                System.out.print("*");
+//                j++;
+//            }
+//            System.out.println("");
+//            i++;
+//        }
 
-            System.out.println("\n");
+        //operations that can be performed with for loop is also possible to do it
+        //with other loops. TRUE
+
+//        practice problem 9
+//        int n = 8;
+//        int sum = 0;
+//        for (int i = 1; i <=10;i++ ) {
+//              sum+=n*i;
+//          }
+//        System.out.println(sum);
+
+        //Practice Problem 10
+        //A do while loop is executed at least once
+
+        //Practice Problem 11 ( problem 2 using while loop)
+
+
+        int sum = 0;
+        int n = 4;
+        int i = 0;
+        while (i<n) {
+            sum = sum + (2*i);
+            i++;
+
         }
+        System.out.print("Sum of even numbers is :");
+        System.out.print(sum);
+
 
 
 
