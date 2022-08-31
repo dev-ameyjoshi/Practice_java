eaaba05173583199199d9ee45cab4acc6995ac5f
Amey Joshi <ameyjoshi2001rj@gmail.com>
2022-08-31 23:16:49 +0530

    Methods ,  Method overloading.

--- src/newcompany/cwh_31_methods.java
+++ src/newcompany/cwh_31_methods.java
@@ -1,6 +1,19 @@
 package newcompany;
 
 public class cwh_31_methods {
+    //The following method associates with the object of the class.
+
+    //if static keyword is not used then
+    // we have to use the method by creating the object of the class
+    // the method is associated with.
+
+    //Access modifiers.
+    // if we don't refer anything then access modifier is default.
+
+    // a method can be called by creating the object of the class in which the method exists.
+    //Example : single fridge in a hostel VS several fridge in a hostel owned by many ppl.
+
+
     static int logic(int x, int y){
         int z;
         if (x > y) {
@@ -13,16 +26,24 @@ public class cwh_31_methods {
         return z;
 
     }
+
+
+
     public static void main(String[] args) {
          int a = 5;
          int b = 10;
          int c;
 
-         c = logic(a,b);
+       //  cwh_31_methods obj = new cwh_31_methods();
+
+       //  c = obj.logic(a,b);
+           c = logic(a,b); // needs static method for main method and the follwing class.
+
          int a1 = 2;
          int b1 = 1;
          int c1;
-         c1 = logic(a1,b1);
+       //  c1 = obj.logic(a1,b1);
+           c1 = logic(a1,b1);
         System.out.println(c);
         System.out.println(c1);
 
---
+++ src/newcompany/methodOverloading_32.java
@@ -0,0 +1,12 @@
+package newcompany;
+
+public class methodOverloading_32 {
+    static  void tellJoke(){
+        System.out.println("I invented a new word!\n" + "Plagiarism!");
+    }
+    // we use void when we don't want to return anything of any type.
+    // 6:41
+    public static void main(String[] args) {
+        tellJoke();
+    }
+}
