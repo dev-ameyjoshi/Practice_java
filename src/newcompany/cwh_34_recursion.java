10d9e220abd57409f4d32e084467b3f90dd499b6
Amey Joshi <ameyjoshi2001rj@gmail.com>
2022-09-03 08:36:08 +0530

    Recursion, Completed fibonacci recursion + iterative.

--- src/newcompany/cwh_34_recursion.java
+++ src/newcompany/cwh_34_recursion.java
@@ -27,10 +27,40 @@ public class cwh_34_recursion {
         }
 
     }
+
+    static int fib(int n){
+        if (n <= 1) {
+            return n;
+        } else {
+            return fib(n-1)+fib(n-2);
+        }
+    }
+    static int fibIterative(int n){
+        if (n == 1) {
+            return 0;
+        }
+        if (n==2) {
+            return 1;
+        }
+        else {
+            int ogno;
+            int num1 = 0;
+            int num2 = 1;
+            for (int i = 1; i < n; i++) {
+                ogno = num1;
+                num1 = num2;
+                num2 = ogno + num1;
+            }
+            return num2;
+        }
+
+    }
     public static void main(String[] args) {
-        int n = 11;
+        int n = 9;
         System.out.println("The value of factorial is : "+factorial(n));
         System.out.println("The value of factorial is : "+iterative(n));
+        System.out.println("The value of Fibonacci is : "+fib(n));
+        System.out.println("The value of Fibonacci is : "+fibIterative(n));
         //Try Fibonacci series
     }
 }
