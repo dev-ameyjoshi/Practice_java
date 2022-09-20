cef48d75f4e97c32d7083cfebf22339bf90e6188
Amey Joshi <ameyjoshi2001rj@gmail.com>
2022-09-20 09:47:29 +0530

    Inheritance and its examples,constructors in inheritance,super keyword with constructors.

--- src/java_programming/cwh_46_constructors_in_inheritance.java
+++ src/java_programming/cwh_46_constructors_in_inheritance.java
@@ -1,39 +1,50 @@
 package java_programming;
 
 
-class Base1{
-    Base1(){
+class Base1 {
+    Base1() {
         System.out.println("I am a constructor!");
     }
-    int x;
-
-    public int getX() {
-        return x;
+    Base1(int x) {
+        System.out.println("I am an overloaded constructor! with value of a as: "+ x);
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
+
 }
 
 class Derived1 extends Base1{
     Derived1(){
-        System.out.println("I am a constructor!");
+        //super(0); runs the constructor with argument from the base class.
+        //with value as 0 for int a.
+        System.out.println("I am a derived class constructor!");
     }
- public int y;
+    Derived1(int x, int y){
+        super(x);
+        System.out.println("I am an overloaded constructor of derived with value of b as: "+ y);
+    }
 
-    public int getY() {
-        return y;
+}
+
+class ChildOfDerived extends Derived1{
+    ChildOfDerived(){
+        System.out.println("I am a child of derived constructor!");
+    }
+    ChildOfDerived(int x, int y, int z){
+        super(x,y);
+        System.out.println("I am an overloaded  constructor of derived with value of z as : "+z);
     }
 
-    public void setY(int y) {
-        this.y = y;
-    }
+
 }
 public class cwh_46_constructors_in_inheritance {
     public static void main(String[] args) {
-        Base1 b = new Base1();
-        Derived1 d = new Derived1();
+        //Base1 b = new Base1();
+        //Derived1 d = new Derived1();
+        //First the base class constructor is
+        //executed then the derived class constructor is executed.
+        //Derived1 d = new Derived1(12,9);
+        //ChildOfDerived cd = new ChildOfDerived();
+        ChildOfDerived cd = new ChildOfDerived(12,13,15);
 
     }
 }
