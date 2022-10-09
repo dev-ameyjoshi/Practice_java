078ecdd35211d71178dc657ec59329db710ea0ae
Amey Joshi <ameyjoshi2001rj@gmail.com>
2022-10-09 17:03:39 +0530

    Abstract Classes VS Interfaces

--- src/java_programming/cwh_54_interface.java
+++ src/java_programming/cwh_54_interface.java
@@ -19,12 +19,47 @@ interface bicycle{
 
 
 interface bicycle{
+    int a = 45;
     void applyBrake(int decrement);
     void speedUp(int increment);
 }
 
+interface blowHorn{
+
+    void blowHornk3g();
+    void blowHornmhn();
+}
+
+
+class AvonCycle implements bicycle,blowHorn{
+    void blowHorn(){
+        System.out.println("Pee Pee poo Poo");
+    }
+   public void applyBrake(int decrement){
+        System.out.println("Applying break!");
+    }
+   public void speedUp(int increment){
+        System.out.println("Increase-ing speed! ");
+    }
+    public void blowHornk3g(){
+        System.out.println("Random 1");
+    }
+    public void blowHornmhn(){
+        System.out.println("Random 2");
+    }
+
+}
 public class cwh_54_interface {
     public static void main(String[] args) {
+        AvonCycle obj = new AvonCycle();
+        obj.applyBrake(3);
+        //You can create properties in interfaces.
+        System.out.println(obj.a);
+        //You can not modify the properties in interfaces as they are final.
+        // obj.a = 454; --> error
 
+
+        obj.blowHornk3g();
+        obj.blowHornmhn();
     }
 }
