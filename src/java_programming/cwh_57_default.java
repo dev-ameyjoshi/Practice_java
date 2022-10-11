63df676c2bc26c922fa72e97b42cfce71a840e45
Amey Joshi <ameyjoshi2001rj@gmail.com>
2022-10-11 07:17:28 +0530

    Default method and calling of private methods.

--- src/java_programming/cwh_57_default.java
+++ src/java_programming/cwh_57_default.java
@@ -4,9 +4,15 @@ package java_programming;
 interface MyCamera{
     void takeSnap();
     void recordVideo();
+
+    private void greet(){
+        System.out.println("Good Morning");
+    } // cant use the private method here unless called in a default method.
  default void record4kVideo(){
+        greet(); // private method can be called here.
      System.out.println("recording 4k vid");
  }
+ //preference will be given to the one written in the class.
 
 
 }
@@ -33,11 +39,11 @@ class MySmartPhone extends MyCellPhone implements MyWifi,MyCamera{
     public void recordVideo(){
         System.out.println("Taking Snap");
     }
-    public void record4kVideo(){
-        System.out.println("recording 4k vid...");
-    }
+//    public void record4kVideo(){
+//        System.out.println("recording 4k vid...");
+//    }//Preference will be given to the one in this class as it is overriding the other method.
     public String[] getNetworks(){
-        System.out.println("Getting List Of Networks");
+        System.out.println("Getting List Of Networks...");
         String[] networksList = {"Harry", "Prashanth", "Anjali5g"};
         return networksList;
     }
@@ -49,7 +55,8 @@ public class cwh_57_default {
     public static void main(String[] args) {
         MySmartPhone ms = new MySmartPhone();
         ms.record4kVideo();
-        String[] ar = ms.getNetworks();
+        //ms.greet(); will throw an error.
+         String[] ar = ms.getNetworks();
         for(String item: ar){
             System.out.println(item);
             //10:15 vid Java Interfaces and Default Methods
